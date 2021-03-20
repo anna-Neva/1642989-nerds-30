84a0e49bab1460445a9bc79c5d74318441cd756c
Your Name <74408496+anna-Neva@users.noreply.github.com>
2021-03-20 12:14:36 +0000

    Поправила кавычки js перед третьей проверкой

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-var popup = document.querySelector('.modal');
-var openPopupButton = document.querySelector('.button-open');
-var closePopupButton = popup.querySelector('.modal-close');
+var popup = document.querySelector(".modal");
+var openPopupButton = document.querySelector(".button-open");
+var closePopupButton = popup.querySelector(".modal-close");
 
-var sliders = document.querySelectorAll('.caption');
-var sliderButtons = document.querySelectorAll('.slider-button');
+var sliders = document.querySelectorAll(".caption");
+var sliderButtons = document.querySelectorAll(".slider-button");
 console.log(sliders);
 
 const form = popup.querySelector(".login-form");
@@ -22,9 +22,9 @@ try {
   isStorageSupport = false;
 }
 
-openPopupButton.addEventListener('click', function (evt) {
+openPopupButton.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.add('modal-show');
+  popup.classList.add("modal-show");
 
   if (storageName) {
       userName.value = storageName;
@@ -42,47 +42,47 @@ openPopupButton.addEventListener('click', function (evt) {
     }
 });
 
-closePopupButton.addEventListener('click', function () {
-  popup.classList.remove('modal-show');
-  popup.classList.remove('modal-error');
-  email.value = '';
-  userName.value = '';
-  letter.value = '';
+closePopupButton.addEventListener("click", function () {
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+  email.value = "";
+  userName.value = "";
+  letter.value = "";
 });
 
-document.addEventListener('keydown', function (evt) {
+document.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
-    popup.classList.remove('modal-show');
+    popup.classList.remove("modal-show");
     loginPopup.classList.remove("modal-error");
-    email.value = '';
-    userName.value = '';
-    letter.value = '';
+    email.value = "";
+    userName.value = "";
+    letter.value = "";
   }
 });
 
 var clearActiveButton = function() {
   sliderButtons.forEach(function(button) {
-    if (button.classList.contains('slider-button-current')) {
-      button.classList.remove('slider-button-current');
+    if (button.classList.contains("slider-button-current")) {
+      button.classList.remove("slider-button-current");
     }
   });
 };
 
 var clearActiveSliders = function() {
   sliders.forEach(function(slider) {
-    if (slider.classList.contains('caption-current')) {
-      slider.classList.remove('caption-current');
+    if (slider.classList.contains("caption-current")) {
+      slider.classList.remove("caption-current");
     }
   });
 };
 
 sliderButtons.forEach(function(button, idx) {
-  button.addEventListener('click', function() {
-    if (!button.classList.contains('slider-button-current')) {
+  button.addEventListener("click", function() {
+    if (!button.classList.contains("slider-button-current")) {
       clearActiveButton();
       clearActiveSliders();
-      button.classList.add('slider-button-current');
-      sliders[idx].classList.add('caption-current');
+      button.classList.add("slider-button-current");
+      sliders[idx].classList.add("caption-current");
     }
   })
 });
